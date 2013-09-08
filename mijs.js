bf6eef3b03260f4c98486d54798e8787c9ef9b0f
VodooPeople <cristian.vodoopeople@gmail.com>
2013-09-08 14:30:35 -0600

    se creo addclass

--- mijs.js
+++ mijs.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 	
+	
+	
+	
 	$('#loaderbutton').click(function() {
 		
 		var $loader = $('<div id="shopping" />').load('mi_archivohtml5.html #box');
 	
 		$loader.appendTo('body');
 		
+	});
+	
+	$('#accion').click(function() {
+	
+		$('.slide01').delay(1000).slideup(500 , function(){
+			$('.slide02').delay(3000).slideup(500 , function(){
+				$('.slide03').delay(3000).slideup(500 , function(){
+					$('.slide04').delay(5000).slideup(500 , function(){
+						$('.slide05').delay(6000).slideup(500);
+					});	
+				});
+			});
+		});
 	});
 	
 });
